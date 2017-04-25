ec18538062d3a7755b5ff6fbc877450b371aa31d
Julie S. Mike <juliesmike@gmail.com>
2017-04-25 11:54:33 -0500

    blazy example

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// (function(){
-//   [].forEach.call(document.querySelectorAll('img[data-src]'),    function(img) {
-//   img.setAttribute('src', img.getAttribute('data-src'));
-//   img.onload = function() {
-//     img.removeAttribute('data-src');
-//   };
-// });
-// })();
-
 window.addEventListener('load', function(){
     var allimages= document.getElementsByTagName('img');
     for (var i=0; i<allimages.length; i++) {
@@ -14,4 +5,8 @@ window.addEventListener('load', function(){
             allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
         }
     }
+
+var bLazy = new Blazy({
+
+});
 }, false)
